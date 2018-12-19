19274e30e08abb99a5b0880b79ababbef136bc9c
Victor Heine Holten <vict1856@edu.eal.dk>
2018-12-19 08:35:39 +0100

    Add project files.

---
+++ Ex39 - Architecture Using Events/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex39___Architecture_Using_Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
